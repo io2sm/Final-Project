cc8794391509285a39e9cc55ddd2bbc75795a3ec
osama <osamak2006@gmail.com>
2026-01-12 03:59:08 +0300

    All Done I Guess v5.0

--- connect.php
+++ connect.php
@@ -2,5 +2,6 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-$con = mysqli_connect("sql305.infinityfree.com","if0_40873143","Kayed2006","if0_40873143_htu_gym"); 
+ $con = mysqli_connect("localhost","root","","htu_gym"); 
+// $con = mysqli_connect("sql305.infinityfree.com","if0_40873143","Kayed2006","if0_40873143_htu_gym"); 
 ?>
--- navbar.php
+++ navbar.php
@@ -3,15 +3,13 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 ?>
-
-
     <link
       href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@500;600;700&display=swap"
       rel="stylesheet"
     />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     
-    <nav>
+    <nav class="regular-nav">
       <div class="logo">
         <img src="Assets/images/Logo.jpg" alt="Logo" />
         <h2>HTU Martial Arts Gym</h2>
